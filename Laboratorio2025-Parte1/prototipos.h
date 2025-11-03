a37bc80f7b8022c7a20f487d56e33a41b1f013c6
Rocío :D <rorocioromero668@gmail.com>
2025-11-03 09:19:51 -0300

    Refactor prototypes in prototipos.h for clarity

--- Laboratorio2025-Parte1/prototipos.h
+++ Laboratorio2025-Parte1/prototipos.h
@@ -1,21 +1,28 @@
 #ifndef PROTOTIPOS_H
 #define PROTOTIPOS_H
-#include "estructuras.h"
 
+void muestroRetorno(TipoRet retorno);
+TipoRet inserto(Archivo &a, Cadena parametros[]);
+TipoRet borroL(Archivo &a, Cadena parametros[]);
+TipoRet muestroT(Archivo a, Cadena parametros[]);
 
-Archivo crearArchivo(char *nombre);//esta permanece inalterada por eso la de tipo ret abajo
+Archivo CrearArchivo(char * nombre);
+TipoRet BorrarArchivo(Archivo &a);
+TipoRet InsertarLinea(Archivo &a, char * version, char * linea, unsigned int nroLinea);
+TipoRet BorrarLinea(Archivo &a, char * version, unsigned int nroLinea);
+TipoRet MostrarTexto(Archivo a, char * version);
 
-//funciones tipo retorno
-TipoRet muestroArchivo(Archivo &a, char *nombre); //lo puse para que ande el coso pero no se si entendi bien
-TipoRet insertarLinea(Archivo &a, char *version, char *linea, unsigned int nroLinea);
-TipoRet borrarLinea(Archivo &a, char * version, unsigned int nroLinea);
-TipoRet borrarArchivo(Archivo &a, char *version);
-TipoRet mostrarTexto(Archivo a, char * version);
+//las tiporet de esta segunda entrega
+TipoRet CrearVersion(Archivo &a, char * version);
+TipoRet MostrarVersiones(Archivo a);
+TipoRet MostrarCambios(Archivo a, char * version);
+TipoRet Iguales(Archivo a, char * version1, char * version2,bool &iguales);
+
+//funciones auxiliares
+Version creoVersion(Version padre, Cadena nombre);
+void muestroVersion(Version v, int espacio);
 
 void help();
-void muestroRetorno(TipoRet retorno);
-bool esVaciaLinea(Linea l);
-bool esVacioArchivo(Archivo a);
-int cantLineas(Archivo a);
-void actualizarLineas(Archivo &a);
+
+
 #endif
