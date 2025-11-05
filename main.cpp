21d975a2d026efc06e4b6d289154d619f958e40f
Rocío :D <rorocioromero668@gmail.com>
2025-11-04 22:42:57 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -34,6 +34,7 @@ int main (int argc, char *argv[]) {
 		if (strcmp(parametros[0],"InsertarLinea")==0) {opcion=insertar;}
 		if (strcmp(parametros[0],"BorrarLinea")==0) {opcion=borrarL;}
 		if (strcmp(parametros[0],"MostrarTexto")==0) {opcion=mostrarT;}
+		if (strcmp(parametros[0],"MostrarVersiones")==0){opcion=mostrarV;}
 		if (strcmp(parametros[0],"ayuda")==0) {opcion=ayuda;}
 		if (strcmp(parametros[0],"limpiar")==0) {opcion=limpiar;}
 		
@@ -49,7 +50,8 @@ int main (int argc, char *argv[]) {
 		case mostrarT:
 			muestroRetorno(muestroT(a,parametros));
 			break;
-			
+		case mostrarV:
+			muestroRetorno(MostrarVersiones(a));
 		case ayuda:
 			help();
 			break;
